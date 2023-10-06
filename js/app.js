9aee95ee4a23dcd6200c2c1d4815ff85e52d616f
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-10-06 17:34:35 +0200

    added file css

--- js/app.js
+++ js/app.js
@@ -7,25 +7,26 @@
 
 for (let i = 0; i < 100; i++) {
     // - inizializzare una variabile che aggiunga il valore +1 al contatore
-    const n = i + 1
+    const n = i + 1;
 
     //     - aggiungere istruzioni condizionali attraverso l'operatore modulo
 
     if (n % 15 === 0) {
         //       - ALTRIMENTI SE il numero è un multiplo di 3 e 5
        //         - stampare in console FizzBuzz  
-        console.log(n, 'FizzBuzz')
+        console.log('FizzBuzz');
     } else if (n % 5 === 0) {
         //       - ALTRIMENTI SE il numero è un multiplo di 5
        //         - stampare in console Buzz
-        console.log(n, 'Buzz')
+        console.log('Buzz');
     } else if (n % 3 === 0) {
         //       - SE il numero è un multiplo di 3
        //         - stampare in console Fizz
-        console.log(n, 'Fizz')
+        console.log('Fizz');
     } else {
         //       - ALTRIMENTI stampare gli altri numeri
-        console.log(n)
+        console.log(n);
     }
-    
+
+        
 }
