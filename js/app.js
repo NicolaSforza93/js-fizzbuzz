7ffae5b7ab8d8d6eb2ae209ade450b23d1fbfb57
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-10-06 21:15:05 +0200

    completed bonus 1 and partially bonus 2

--- js/app.js
+++ js/app.js
@@ -5,28 +5,45 @@
 //     - impostare una condizione da verificare per poter eseguire il codice
 //     - incrementare il valore del contatore 
 
+const gridDomElement = document.querySelector('.grid')
+console.log('gridDomElement')
+
 for (let i = 0; i < 100; i++) {
     // - inizializzare una variabile che aggiunga il valore +1 al contatore
     const n = i + 1;
 
     //     - aggiungere istruzioni condizionali attraverso l'operatore modulo
-
+    let output = 0; 
     if (n % 15 === 0) {
+        output = 'fizzBuzz';
         //       - ALTRIMENTI SE il numero è un multiplo di 3 e 5
        //         - stampare in console FizzBuzz  
-        console.log('FizzBuzz');
+        //  console.log('FizzBuzz');
     } else if (n % 5 === 0) {
+        output = 'buzz';
         //       - ALTRIMENTI SE il numero è un multiplo di 5
        //         - stampare in console Buzz
-        console.log('Buzz');
+        // console.log('Buzz');
     } else if (n % 3 === 0) {
+        output = 'fizz';
         //       - SE il numero è un multiplo di 3
        //         - stampare in console Fizz
-        console.log('Fizz');
+        // console.log('Fizz');
     } else {
         //       - ALTRIMENTI stampare gli altri numeri
-        console.log(n);
+        // console.log(n);
     }
+
+    const boxDomElement = document.createElement('div');
+    boxDomElement.classList.add('box');
+    boxDomElement.innerHTML = output || n
+    console.dir(boxDomElement);
+    console.log(output || n);
+    gridDomElement.append(boxDomElement);
+
+    boxDomElement.style.backgroundColor = "#3F87AE"; 
+    boxDomElement.style.Color = "black";
+
 
         
 }
