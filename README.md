2f4d5aec7fdbe4ed7d14094d9544b483d2818267
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-10-06 16:31:07 +0200

    added readme complete and program js

---
+++ README.md
@@ -0,0 +1,19 @@
+Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+## FizzBuzz
+
+- scrivere un programma che stampi i numeri da 1 a 100
+  - aprire un ciclo for
+    - creare il contatore
+      - impostare una variabile e assegnare un valore iniziale
+    - impostare una condizione da verificare per poter eseguire il codice
+    - incrementare il valore del contatore 
+    - inizializzare una variabile che aggiunga il valore +1 al contatore
+    - aggiungere istruzioni condizionali attraverso l'operatore modulo
+      - SE il numero è un multiplo di 3
+        - stampare in console Fizz
+      - ALTRIMENTI SE il numero è un multiplo di 5
+        - stampare in console Buzz
+      - ALTRIMENTI SE il numero è un multiplo di 3 e 5
+        - stampare in console FizzBuzz
+      - ALTRIMENTI stampare gli altri numeri
---
+++ js/app.js
@@ -0,0 +1,31 @@
+// - scrivere un programma che stampi i numeri da 1 a 100
+//   - aprire un ciclo for
+//     - creare il contatore
+//       - impostare una variabile e assegnare un valore iniziale
+//     - impostare una condizione da verificare per poter eseguire il codice
+//     - incrementare il valore del contatore 
+
+for (let i = 0; i < 100; i++) {
+    // - inizializzare una variabile che aggiunga il valore +1 al contatore
+    const n = i + 1
+
+    //     - aggiungere istruzioni condizionali attraverso l'operatore modulo
+
+    if (n % 15 === 0) {
+        //       - ALTRIMENTI SE il numero è un multiplo di 3 e 5
+       //         - stampare in console FizzBuzz  
+        console.log(n, 'FizzBuzz')
+    } else if (n % 5 === 0) {
+        //       - ALTRIMENTI SE il numero è un multiplo di 5
+       //         - stampare in console Buzz
+        console.log(n, 'Buzz')
+    } else if (n % 3 === 0) {
+        //       - SE il numero è un multiplo di 3
+       //         - stampare in console Fizz
+        console.log(n, 'Fizz')
+    } else {
+        //       - ALTRIMENTI stampare gli altri numeri
+        console.log(n)
+    }
+    
+}
